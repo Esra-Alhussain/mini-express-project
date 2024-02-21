1402e2b982a56d4662717db0a535de2b806ae19c
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-02-20 16:02:49 -0800

    create the country model

---
+++ models/Country.js
@@ -0,0 +1,16 @@
+const { DataType } = require('sequelize');
+const sequelize = require('../config');
+
+const Country = sequelize.define('Country', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = Country;
