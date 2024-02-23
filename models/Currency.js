e2282a8581154ccc639a6b6d65308cbe68c815bd
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-02-23 09:43:25 -0800

    add an association (one-to-one) in Currency model

--- models/Currency.js
+++ models/Currency.js
@@ -1,7 +1,10 @@
 
 //import DataTypes object by extracting it from Sequelize package
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config');  //import sequelize instance from the configuration file
+const { DataTypes, Model } = require('sequelize');
+//import the Sequelize instance from the configuration file
+const sequelize = require('../config');  
+
+class Currency extends Model {}
 
 const Currency = sequelize.define('Currency', {  //define the Currency model
     id: {
@@ -20,7 +23,14 @@ const Currency = sequelize.define('Currency', {  //define the Currency model
     ConversionRate: {
         type: DataTypes.FLOAT,
         allowNull: false
-    }
-});
+    },
+  },
+    {
+        sequelize,   //specifies the Sequelize instance to be used for this model
+        modelName: 'Currency'   //sets the name of the model to 'Currency
+    });
 
+    const Country = require('./Country'); // import the Country model 
+    //'foreignKey' specifies the foreign key coloumn (countryId) in the (Currency) table that references rge primary key of the country table 
+    Currency.belongsTo(Country, { foreignKey: 'countryId' })
 module.exports =Currency;
